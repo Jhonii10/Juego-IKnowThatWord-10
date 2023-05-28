aed440a7a1e9ee0ecd15fda1a7979fb45ae74417
Jhonni10 <jhonieripianoscue@gmail.com>
2023-05-28 13:02:26 -0500

    creacion de construtor users

--- src/iKnowThatWord/Users.java
+++ src/iKnowThatWord/Users.java
@@ -1,4 +1,26 @@
 package iKnowThatWord;
 
+import java.util.ArrayList;
+
 public class Users {
+
+    private ArrayList<String> usuarios, editarUsuarios = new ArrayList<String>();
+    private int unUsuario, nivelUsuario;
+    private String nombreUsuario;
+    private Register register;
+
+    /**
+     * Constructor
+     */
+
+    public Users() {
+        register = new Register();
+        usuarios = register.lecturaUsuario();
+
+        unUsuario = 0;
+        nivelUsuario = 1;
+        nombreUsuario = " ";
+    }
+
+
 }
