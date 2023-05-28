0a9060239e8c6efc02e6e2228b468d4c48715290
Jhonni10 <jhonieripianoscue@gmail.com>
2023-05-28 12:22:06 -0500

    Creacion de funcionalidades de clase register

--- src/iKnowThatWord/Vocabulary.java
+++ src/iKnowThatWord/Vocabulary.java
@@ -1,4 +1,13 @@
 package iKnowThatWord;
 
+import java.util.ArrayList;
+
 public class Vocabulary {
+    private ArrayList<String> vocabulary = new ArrayList<>();
+    private ArrayList<String> levelWords;
+
+    public Vocabulary(){
+        vocabulary =
+    }
+
 }
