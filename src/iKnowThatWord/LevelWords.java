0fcab26d82ccb70f19e19f9acf6650ed4fabb631
MoonLoveless <lexyls523@gmail.com>
2023-05-29 21:23:36 -0500

    haciendo el constructor de la clase LevelWords

--- src/iKnowThatWord/LevelWords.java
+++ src/iKnowThatWord/LevelWords.java
@@ -1,4 +1,20 @@
 package iKnowThatWord;
 
+import java.util.ArrayList;
+import java.util.random;
+
 public class LevelWords {
+    private Vocabulary vocabulary;
+    private ArrayList<String> todasLasPalabras, palabrasDelNivel, palabrasAMemorizar;
+
+    /**
+     * Constructor
+     */
+
+    public PalabrasDelNivel(){
+        vocabulary = new Vocabulary();
+        todasLasPalabras = vocabulary.getVocabulary();
+        palabrasDelNivel = new ArrayList<>();
+        palabrasAMemorizar = new ArrayList<>();
+    
 }
